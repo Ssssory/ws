b0f85119c436685847b7fc571e7db245b53b1a2e
Ssssory <itrubchaninov@list.ru>
2018-03-25 13:57:21 +0300

    reconnect DB and add gitignore

--- src/Chat.php
+++ src/Chat.php
@@ -2,44 +2,86 @@
 namespace MyApp;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
-use MyApp\model\DataBase;
 
 class Chat implements MessageComponentInterface {
     protected $clients;
+    protected $DB;
 
     public function __construct() {
         $this->clients = new \SplObjectStorage;
+        $this->DB = new DataBase();
     }
 
     public function onOpen(ConnectionInterface $conn) {
         // Store the new connection to send messages to later
+        $this->checkDB();
         $this->clients->attach($conn);
+        $this->DB->putQuery("INSERT INTO `users` (name,res_id,last) VALUES('anonim','".$conn->resourceId."','')");
 
         echo "New connection! ({$conn->resourceId})\n";
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
-//        print_r($msg);
         $msg = json_decode($msg,true);
-        $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg['name'], $numRecv, $numRecv == 1 ? '' : 's');
-        echo $msg['to'];
-        foreach ($this->clients as $client) {
+        $this->checkDB();
+        if(empty($msg['hash'])){
+            if(empty($msg['name'])){
+                $from->send('error');
+            }else{
+                $tempStrQuery = "UPDATE `users` SET name = '".$msg['name']."',last='".md5($msg['name'].time())."' WHERE res_id = '".$from->resourceId."'";
+               $this->DB->putQuery($tempStrQuery); 
+            }
+        }else{
+            // проверка на адекватность хэша
+            
+            if(!empty($msg['to'])){
+                $curId = $this->DB->getRow("SELECT res_id FROM `users` WHERE name = '".$msg['to']."'");
+                var_export($curId['res_id']);
+                foreach ($this->clients as $client) {
+                    if($client->resourceId == $curId['res_id']){
+                        $client->send($msg['text']);
+                    }
+                }                
+            }
+            if(!empty($msg['to_group'])){
+                foreach ($this->clients as $client) {
+                    if($client->resourceId != $from->resourceId){
+                        $client->send($msg['text']);
+                    }
+                } 
+            }
+            if(!empty($msg['all'])){
+                foreach ($this->clients as $client) {
+                    if ($from !== $client) {             
+                        $client->send($msg['text']);
+                          }
+                      }
+            }
+//            if(!empty($msg['hash'])){
+//                
+//            }
+            
+        }
+//        $numRecv = count($this->clients) - 1;
+//        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+//            , $from->resourceId, $msg['name'], $numRecv, $numRecv == 1 ? '' : 's');
+//        echo $msg['to'];
+//        foreach ($this->clients as $client) {
 //            if ($from !== $client) {
 //                // The sender is not the receiver, send to each client connected
 //                $client->send($msg['name']);
 //            }
-            if($client->resourceId == $msg['to']){
-                $client->send($msg['text']);
-            }
-        }
+//            if($client->resourceId == $msg['to']){
+//                $client->send($msg['text']);
+//            }
+//        }
     }
 
     public function onClose(ConnectionInterface $conn) {
         // The connection is closed, remove it, as we can no longer send it messages
         $this->clients->detach($conn);
-
+        $this->checkDB();
+        $this->DB->execQuery("DELETE FROM `users` WHERE `res_id`= ".$conn->resourceId);
         echo "Connection {$conn->resourceId} has disconnected\n";
     }
 
@@ -47,5 +89,13 @@ class Chat implements MessageComponentInterface {
         echo "An error has occurred: {$e->getMessage()}\n";
 
         $conn->close();
+    }
+    
+    public function checkDB(){
+        if(!$this->DB->isConnect()){
+            unset($this->DB);
+            $this->DB = new DataBase();
+            echo "reconnect\n";
+        }
     }
 }
--- src/model/DB.php
+++ src/model/DB.php
@@ -7,14 +7,14 @@ class DataBase {
     protected   $datab;
 
     #   Подключение
-    public function __construct( $userName = "root", $password = "", $host = "localhost", $dbName = "ws", $options = array()) {
+    public function __construct( $userName = "root", $password = "", $host = "localhost", $dbName = "ws", $options = array(\PDO::ATTR_PERSISTENT => true)) {
     #Проверка подключения
 		$this->isConn = TRUE;
         try {
-            $this->datab = new PDO( "mysql:host={$host};dbname={$dbName};charset=utf8", $userName, $password, $options );
-            $this->datab->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            $this->datab = new \PDO( "mysql:host={$host};dbname={$dbName};charset=utf8", $userName, $password, $options );
+            $this->datab->setAttribute( \PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION );
             #Атрибут для первоначальной настройки
-            $this->datab->setAttribute( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC );
+            $this->datab->setAttribute( \PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC );
         } catch ( PDOException $e ) {
 			#Показать ошибку, если нет подключения
             throw new Exception( $e->getMessage() );
@@ -27,6 +27,36 @@ class DataBase {
     public function Disconnect() {
         $this->datab   =   NULL;
         $this->isConn  =   FALSE;
+    }    
+    
+    #-------------------------------------------------------------------------------------------------------------------
+    #   Чек соединения с базой
+    #-------------------------------------------------------------------------------------------------------------------
+    public function isConnect() {
+        if (!is_null($this->datab)){
+//             try {
+//                $testArray = $this->getRow('SELECT 1+2+3');
+//                 $testRes = $this->datab->query( 'SELECT 1+2+3' );
+//                 $testArray = $testRes->fetch();
+//                 var_export($testArray);
+//                if ($testArray['1+2+3'] == 6) {
+//                    return true;
+//                }
+//            }
+//            catch (PDOException $e) {
+//                return false;
+//            }
+            try {
+                $this->datab->query("SHOW STATUS;")->execute();
+                return true;
+            } catch(\PDOException $e) {
+                if($e->getCode() != 'HY000' || !stristr($e->getMessage(), 'server has gone away') ) {
+                    throw $e;
+                }
+                return false;
+            }
+        }
+        return false;
     }
 
     #-------------------------------------------------------------------------------------------------------------------
@@ -60,9 +90,9 @@ class DataBase {
     }
 
     #-------------------------------------------------------------------------------------------------------------------
-    #   Запись
+    #   Выполнение произвольного запроса
     #-------------------------------------------------------------------------------------------------------------------
-    public function insertRow( $query, $params = array() ) {
+    public function putQuery( $query, $params = array() ) {
 		try {
     			$stmt = $this->datab->prepare( $query );
                 $stmt->execute( $params );
@@ -73,19 +103,13 @@ class DataBase {
 	}
 
 
+    
+    
     #-------------------------------------------------------------------------------------------------------------------
-    #   Обновление данных
+    #   Выполнение произвольного запроса без экранирования
     #-------------------------------------------------------------------------------------------------------------------
-    public function updateRow( $query, $params = array() ) {
-        $this->insertRow( $query, $params );
-    }
 
-    #-------------------------------------------------------------------------------------------------------------------
-    #   Удаление
-    #-------------------------------------------------------------------------------------------------------------------
-    public function deleteRow( $query, $params = array() ) {
-        $this->insertRow( $query, $params );
-    }
+
 
     public function execQuery( $query) {
     try {
